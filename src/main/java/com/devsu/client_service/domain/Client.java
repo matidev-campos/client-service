a240704cd0ad6aadd146e4f3b0e80070bacd591c
matidev-campos <matidev.campos@gmail.com>
2025-12-17 08:03:14 -0300

    feat(domain): add person and client entities with join inheritence

---
+++ src/main/java/com/devsu/client_service/domain/Client.java
@@ -0,0 +1,46 @@
+package com.devsu.client_service.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+@PrimaryKeyJoinColumn(name = "person_id")
+public class Client extends Person {
+
+    @Column(name = "client_id", nullable = false, unique = true)
+    private String clientId;
+    @Column(nullable = false, unique = true)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private Boolean statement;
+
+    public Client() {
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getStatement() {
+        return statement;
+    }
+
+    public void setStatement(Boolean statement) {
+        this.statement = statement;
+    }
+}
