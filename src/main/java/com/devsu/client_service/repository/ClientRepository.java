3d01bb06aad23765f0791be429d3364f9fb412f2
matidev-campos <matidev.campos@gmail.com>
2025-12-17 09:22:10 -0300

    feat(repository): add ClientRepository using Spring Data JPA

---
+++ src/main/java/com/devsu/client_service/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.devsu.client_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.devsu.client_service.domain.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
