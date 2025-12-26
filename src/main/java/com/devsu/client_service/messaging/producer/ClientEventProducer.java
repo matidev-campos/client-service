8b4448b58c1563b77f9675df7b23378ad8ba9351
matidev-campos <matidev.campos@gmail.com>
2025-12-26 15:27:04 -0300

    Add fixes in Client entity and fixes in config service for async communication

--- src/main/java/com/devsu/client_service/messaging/producer/ClientEventProducer.java
+++ src/main/java/com/devsu/client_service/messaging/producer/ClientEventProducer.java
@@ -6,10 +6,16 @@ import com.devsu.client_service.messaging.event.ClientDeletedEvent;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.UUID;
 
 @Component
 public class ClientEventProducer {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(ClientEventProducer.class);
 
     private final RabbitTemplate rabbitTemplate;
 
@@ -18,8 +24,16 @@ public class ClientEventProducer {
     }
 
     public void sendClientCreated(UUID clientId, String name, String identification) {
+
         ClientCreatedEvent event =
                 new ClientCreatedEvent(clientId, name, identification);
+
+        log.info(
+                "Publishing ClientCreatedEvent | clientId={} | exchange={} | routingKey={}",
+                clientId,
+                RabbitMQConfig.CLIENT_EVENTS_EXCHANGE,
+                "client.created"
+        );
 
         rabbitTemplate.convertAndSend(
                 RabbitMQConfig.CLIENT_EVENTS_EXCHANGE,
@@ -29,6 +43,12 @@ public class ClientEventProducer {
     }
 
     public void sendClientDeleted(UUID clientId) {
+        log.info(
+                "📤 Publishing ClientDeletedEvent | clientId={} | exchange={} | routingKey={}",
+                clientId,
+                RabbitMQConfig.CLIENT_EVENTS_EXCHANGE,
+                "client.deleted"
+        );
         rabbitTemplate.convertAndSend(
                 RabbitMQConfig.CLIENT_EVENTS_EXCHANGE,
                 "client.deleted",
